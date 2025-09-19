5aca046bcad244a2e5a2e16babedf37bb8d5a2da
Joaquim Pereira <joelpereirasegunda@gmail.com>
2025-09-19 18:06:57 +0100

    adicionado o exercício 072

---
+++ tuples/072.py
@@ -0,0 +1,9 @@
+numberText = (
+    'zero','one','two','three','four','five',
+    'six','seven','eight','nine','ten','eleven',
+    'twelve','thirteen','fourteen','fifteen','sixteen','seventeen',
+    'eighteen','nineteen','twenty'
+)
+
+number = int(input("Insira um valor de (0 - 20): "))
+print(f"O número digitado é: {numberText[number]}")
