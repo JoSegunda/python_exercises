d8910c8abb9d12828fca8dc4fb309f9d1849163d
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-09-21 22:18:40 +0100

    Added exercise 075.py

---
+++ tuples/075.py
@@ -0,0 +1,20 @@
+
+listOfNumbers = []
+evenNumbers = []
+n = 0
+while n != 4:
+    temp = int(input("Digite um valor: "))
+    listOfNumbers.append(temp)
+    if(temp%2==0):
+        evenNumbers.append(temp)
+    n += 1
+    
+tupleOfNumbers = tuple(listOfNumbers)
+print(f"O valor 9 ocorreu na posição {tupleOfNumbers.count(9)}")
+try:
+    print(f" O primeiro valor 3 aparece na {tupleOfNumbers.index(3)}ª posição.")
+except:
+    print("Valor não 3 está presente.")
+
+
+print(f"Números pares: {evenNumbers}")
