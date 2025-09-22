0fe4aa2a78a4d7c3fa69a2b1f9817624416f2ac9
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-09-22 20:29:00 +0100

    exercise description added

---
+++ tuples/077.py
@@ -0,0 +1,15 @@
+palavras = (
+    "casa", "mesa", "livro", "porta", "janela",
+    "carro", "rua", "bola", "tempo", "pessoa",
+    "animal", "planta", "rio", "cidade", "escola",
+    "computador", "telefone", "chave", "papel", "amigo"
+)
+
+for word in palavras:
+    print(f"{word} - ",end="")
+
+    for n in word:
+        if(n in "aeiou"):
+            print(n, end=" ")
+    
+    print("\n")
