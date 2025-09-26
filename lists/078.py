78893bf81b02f88add7e5bd7b63856298eb199c2
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-09-26 17:47:58 +0100

    New exercises added

---
+++ lists/078.py
@@ -0,0 +1,3 @@
+newList = [int(input(f"Digite um valor: ")) for c in range(0, 5)]
+print(f"Maior: {max(newList)}, Posição {newList.index(max(newList))}")
+print(f"Maior: {min(newList)}, Posição {newList.index(min(newList))}")
