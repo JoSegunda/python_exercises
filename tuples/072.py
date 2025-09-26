7f127922bd4ac2d07718051e72a5d05ddc705f6f
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-09-26 17:11:44 +0100

    change in tuples

--- tuples/072.py
+++ tuples/072.py
@@ -8,5 +8,5 @@ while True:
     number = int(input("Insira um valor de (0 - 20): "))
     if(number <= 20 and number >= 0):
         break
-
+    print("Tente novamente")
 print(f"O número digitado é: {numberText[number]}")
