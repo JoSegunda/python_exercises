b73c6f5fc4bf56da25e48f6fac0a830d27b2c61b
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-07 23:15:18 +0100

    changed exercise 81

--- lists/081.py
+++ lists/081.py
@@ -1,8 +1,10 @@
 numbers = []
-
+numbersCount = 0
 while True:
     numbers.append(int(input("DIgite um número: ")))
     answer = input("Quer continuar [S / N]")
 
     if answer in "nN":
         break
+    
+    numbersCount += 1
