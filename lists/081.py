6250e23d347d28c7346eebde813f2ffb20205517
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-07 23:23:47 +0100

    changed exercise 81

--- lists/081.py
+++ lists/081.py
@@ -1,14 +1,14 @@
 numbers = []
 numbersCount = 0
 while True:
-    numbers.append(int(input("DIgite um número: ")))
-    answer = input("Quer continuar [S / N]")
+    numbers.append(int(input("Digite um número: ")))
+    answer = input("Quer continuar [S / N]: ")
+    numbersCount += 1
 
     if answer in "nN":
         break
     
-    numbersCount += 1
-
+numbers.reverse()
 print(f"Foram digitados {numbersCount} números.")
-print(f"Valores em ordem decrescente: {numbers.reverse()}")
+print(f"Valores em ordem decrescente: {numbers}")
 print("O valor 5 ","está na lista" if 5 in numbers else "não está na lista")
