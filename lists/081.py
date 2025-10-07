04587a0c2d408a666f0dba37222d5eb55abfce46
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-07 23:17:41 +0100

    changed exercise 81

--- lists/081.py
+++ lists/081.py
@@ -8,3 +8,7 @@ while True:
         break
     
     numbersCount += 1
+
+print(f"Foram digitados {numbersCount} números.")
+print(f"Valores em ordem decrescente: {numbers.reverse()}")
+print("O valor 5 ","está na lista" if 5 in numbers else "não está na lista")
