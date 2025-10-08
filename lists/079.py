a3d3dc66d5b7bb6b8f0be9300413a7fb6fd7d420
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-08 13:57:11 +0100

    changed exercises

--- lists/079.py
+++ lists/079.py
@@ -4,6 +4,8 @@ while True:
     n = int(input("Digite um valor: "))
     if(n not in newList):
         newList.append(n)
+    elif n in newList:
+        print("Valor duplicado! não vou adicionar")
     
     val = str(input("Quer continuar? [S / N] "))
     if(val in "nN"):
