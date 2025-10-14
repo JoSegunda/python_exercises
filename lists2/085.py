f0c819f9a8e778adfac79a1d959d527371d14712
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-14 22:44:00 +0100

    change in 085.py

--- lists2/085.py
+++ lists2/085.py
@@ -1,1 +1,13 @@
-print("Hello world")
+numbers = [[],[]]
+
+for i in range(7):
+    n = int(input("Digite um número: "))
+
+    if n%2==0:
+        numbers[0].append(n)
+    else:
+        numbers[1].append(n)
+
+print("=="*20)
+print(f"Valores Pares: {sorted(numbers[0])}")
+print(f"Valores Ímpares: {sorted(numbers[1])}")
