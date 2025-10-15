02f1456428e5ef0e7bdd8d14c27c738e586a42e0
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-15 14:16:49 +0100

    new exercise 088.py

---
+++ lists2/088.py
@@ -0,0 +1,13 @@
+from random import sample
+from time import sleep
+palpite = int(input("Quantos jogos serão jogados? "))
+storeValues = []
+tempList = []
+print("=-"*4,f"Sorteando {palpite} jogos", "=-"*4)
+
+for i in range(palpite):
+    print(f"Jogo {i+1}: ", end=" ")
+    storeValues.append(sample(range(1, 61), 6))
+    print(storeValues[i])
+    sleep(1)
+print("-="*5,"< BOA SORTE > ","-="*5)
