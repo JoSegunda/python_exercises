14ec294e8f3a65b65ff3d24c1dcc06b15ba310eb
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-29 21:46:18 +0000

    Updated exercise 93

--- dictionaries/093.py
+++ dictionaries/093.py
@@ -1,21 +1,34 @@
 player = dict()
+players= list()
 goals = []
-player['Nome'] = input("Nome do Jogador: ")
-n = int(input(f"Quantas partidas {player['Nome']} jogou: "))
-print("=="*20)
-for i in range(1, n+1):
-    goals.append(int(input(f"Quantos gols na partida {i}? ")))
-
-player['Goals'] = goals
-player['Total'] = sum(goals)
+stopCondition = "y"
 print("=="*20)
 
-for k,v in player.items():
-    print(f"{k}: {v}")
-print("=="*20)
-print(f"O jogador {player['Nome']} jogou {n} partidas.")
 
-for j in range(len(player['Goals'])):
-    print(f"    Na partida {j+1}, fez {player['Goals'][j]} gols.")
 
-print(f"Foi um total de {player['Total']} gols.")
+
+
+
+while stopCondition not in "nN":
+    name = input("Nome do Jogador: ")
+ 
+    n = int(input(f"Quantas partidas {player[name]} jogou: "))
+    for i in range(n):
+        goals.append(int(input(f"Quantos gols na partida {i+1}? ")))
+
+    player[name] = goals[:]
+    del goals
+
+# player['Goals'] = goals
+# player['Total'] = sum(goals)
+# print("=="*20)
+
+# for k,v in player.items():
+#     print(f"{k}: {v}")
+# print("=="*20)
+# print(f"O jogador {player['Nome']} jogou {n} partidas.")
+
+# for j in range(len(player['Goals'])):
+#     print(f"    Na partida {j+1}, fez {player['Goals'][j]} gols.")
+
+# print(f"Foi um total de {player['Total']} gols.")
