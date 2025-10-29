ec2a13894949c339bae360a4e5a57cbea2e2ee80
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-29 22:50:56 +0000

    Revert "Updated readme"
    This reverts commit 985ef6fbdded8bc493783acae13b98aa416a9356.

--- dictionaries/093.py
+++ dictionaries/093.py
@@ -1,47 +1,24 @@
 player = dict()
 players= list()
 goals = []
-stopCondition = "y"
+print("=="*20)
+for i in range():
+    player['Nome'] = input("Nome do Jogador: ")
+    n = int(input(f"Quantas partidas {player['Nome']} jogou: "))
+    goals.append(int(input(f"Quantos gols na partida {i}? ")))
+
+    players.append()
+
+player['Goals'] = goals
+player['Total'] = sum(goals)
 print("=="*20)
 
-
-
-
-
-
-while stopCondition not in "nN":
-    name = input("Nome do Jogador: ")
- 
-    n = int(input(f"Quantas partidas {name} jogou: "))
-    for i in range(n):
-        goals.append(int(input(f"Quantos gols na partida {i+1}? ")))
-
-    stopCondition = input("Quer continuar? [S / N]: ")
-    player[name] = goals.copy()
-    goals = []
-
-print("==="*20)
-print("Cod  Nome         golos              total")
-print("---"*20)
-
-i = 1
 for k,v in player.items():
-    print(f"{i:<5}{k:<12}{str(v):<20}{sum(v):>5}")
-    i += 1
-print("---"*20)
+    print(f"{k}: {v}")
+print("=="*20)
+print(f"O jogador {player['Nome']} jogou {n} partidas.")
 
-show = int(input("Mostrar dados de qual jogador? "))
-while show != 999:
-    
-    if(show > len(player) or show <= 0):
-        print("Tente Novamente")
-    else:
-        name = list(player.keys())[show-1]
-        print(f"-- Levantamento do jogador {name}")
-        for i in range(len(player[name])):
-            print(f"    No jogo {i+1} fez {player[name][i]} gols.")
-        print("--"*20)
-    
-    show = int(input("Mostrar dados de qual jogador? "))
+for j in range(len(player['Goals'])):
+    print(f"    Na partida {j+1}, fez {player['Goals'][j]} gols.")
 
-print("<<   VOLTE SEMPRE     >>")
+print(f"Foi um total de {player['Total']} gols.")
