a271eb37f83b7f9d84cedcbd31566a2df1b007cb
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-29 22:15:43 +0000

    Updated exercise 93

--- dictionaries/093.py
+++ dictionaries/093.py
@@ -29,6 +29,15 @@ for k,v in player.items():
     print(f"{i:<5}{k:<12}{str(v):<20}{sum(v):>5}")
     i += 1
 print("---"*20)
+
+stopCondition = " "
+while stopCondition not in "nN":
+    show = int(input("Mostrar dados de qual jogador? "))
+    if(show > len(player)):
+        print("Tente Novamente")
+    print(f"--Levantamento do jogador {player[show-1]}")
+    for i in range(player[show-1]):
+        print(f" No jogo {i+1} fez {player[show-1][i]} gols.")
 # for j in range(len(player['Goals'])):
 #     print(f"    Na partida {j+1}, fez {player['Goals'][j]} gols.")
 
