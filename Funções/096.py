c3405418457d02dc80a506f838fca76705748b72
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-10-30 14:41:39 +0000

    updated exe 096

---
+++ Funções/096.py
@@ -0,0 +1,9 @@
+def area(largura, compr):
+    print(f"area = {largura * compr}m²")
+
+
+print("controle de terrenos")
+print("--"*20)
+altura = float(input("Altura: "))
+comprimento = float(input("Comprimento: "))
+area(altura, comprimento)
