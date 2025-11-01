8810d4c3f41cd5c108e905fbd8e63b352c6d4c6c
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-01 21:11:11 +0000

    updated exe 98

--- Funções/098.py
+++ Funções/098.py
@@ -1,8 +1,14 @@
+from time import sleep
+
 def contador(start, end, step):
     if(start > end):
         step = -step
         end -= 2
+    print("=="*20)
     for i in range(start, end+1,step):
-        print(i)
+        print(i, sep=" ")
+        sleep(1)
+    print("FIM")
 
+contador(10,0,2)
 contador(10,0,2)
