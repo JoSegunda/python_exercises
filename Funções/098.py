dfb83f8b305be55e08944e46a4c62eb60097b933
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-01 21:15:27 +0000

    updated exe 98

--- Funções/098.py
+++ Funções/098.py
@@ -8,6 +8,7 @@ def contador(start, end, step):
     for i in range(start, end+1,step):
         print(i, end=" ")
         sleep(1)
+        print
     print("=="*20)
     print("FIM")
 
@@ -15,7 +16,7 @@ contador(1,10,1)
 contador(10,0,2)
 
 print("Agora é sua vez de personalizar a contagem:")
-start = int(input("Início"))
-end = int(input("Fmm:"))
-passo = int(input("Passo:"))
+start = int(input("Início: "))
+end = int(input("Fmm: "))
+passo = int(input("Passo: "))
 contador(start,end,passo)
