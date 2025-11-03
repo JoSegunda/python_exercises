3344cdabe4b6f7719575685deed61710e8e6d87b
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-03 12:41:58 +0000

    updated readme

--- Funções 2/101.py
+++ Funções 2/101.py
@@ -4,8 +4,11 @@ from datetime import datetime
 def voto(data):
     ano = datetime.now().year - data
     print(ano)
-    if()
+    
+    if(ano > 18 and ano < 65): return ("Voto obrigatório")
+    elif(ano > 65): return ("Voto opcional")
+    else: return ("Não vota")
 
 
 ano = int(input("Ano de nascimento: "))
-voto(2000)
+print(voto(ano))
