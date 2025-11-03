fb5bdf030fbc23d57b0fb43d983f6578c72f8800
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-03 12:19:58 +0000

    updated exercise 97

--- Funções/099.py
+++ Funções/099.py
@@ -10,10 +10,11 @@ def maior(*numbers):
         if number > n:
             n = number
     print(f"Foram informados {len(numbers)} valores")
-    print(f"Maior valor: {n}")
+    print(f"Maior valor: {n}" if n > 0 else "O maior valor não existe")
     
 
 maior(2,9,4,5,7,1)
 maior(4, 7,0)
 maior(1,2)
 maior(6)
+maior(0)
